b0a8425c5c33bdb48d1fa3d92356e817e86176c1
timhaha <35909218+timhaha@users.noreply.github.com>
2019-12-08 22:52:41 -0500

    Add files via upload

---
+++ about.php
@@ -0,0 +1,81 @@
+<html>
+<head>
+
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+</head>
+<body>
+<a href="index.php"><img id="icon" src="img/logo.jpg" height=100></a>
+<ul id="liulan">
+  <li><a href="index.php">Home</a></li>
+  <li><a href="reserve.php">Reservation</a></li>
+  <li><a href="about.php">About us</a></li>
+  <li><a href="admin.php">Admin</a></li>
+</ul>
+	<table id="about" align="center">
+            		<tr>
+            			<td rowspan="6"><img src="img/Xingyu Sheng.png" alt="member1.jpg" height=300></td>
+            			<td><strong>Sharon Heloton</strong></td>
+            			<td rowspan="6"><img src="img/Yinan Wang.png" alt="member2.jpg" height=300></td>
+            			<td><strong>Lucy Lee</strong></td>
+            		</tr>
+            		<tr>
+            			<td>Sharon Helton takes charge in reserve page also confirmation page, all pages� header and footer. </td>
+            			<td>Lucy Lee takes charge in reserve page also confirmation page, all pages� header and footer. </td>
+            		</tr>
+            		<tr>
+            			<td></td>
+            			<td></td>
+            		</tr>
+            		<tr>
+            			<td></td>
+            			<td></td>
+            		</tr>
+            		<tr>
+            			<td></td>
+            			<td></td>
+            		</tr>
+            		<tr>
+            			<td></td>
+            			<td></td>
+            		</tr>
+            		<tr>
+            			<td rowspan="6"><img src="img/Ge Song.png" alt="member3" height=300></td>
+            			<td><strong>Robert Mogan</strong></td>
+            			<td rowspan="6"><img src="img/Litong Xu.png" alt="member4" height=300></td>
+            			<td><strong>Lisa Stellar</strong></td>
+            		</tr>
+            		<tr>
+            			<td>Robert Mogan mainly takes responsible for the about us page and also confirmation page, all pages� hearer and footer. </td>
+            			<td>Lisa Stellar mainly in charge of the home page and also confirmation page, all pages� header and footer. </td>
+            		</tr>
+            		<tr>
+            			<td></td>
+            			<td></td>
+            		</tr>
+            		<tr>
+            			<td></td>
+            			<td></td>
+            		</tr>
+            		<tr>
+            			<td><p class="addtocart"></p></td>
+            			<td><p class="addtocart"></p></td>
+            		</tr>
+            		<tr>
+            			<td></td>
+            			<td></td>
+            		</tr>
+            	</table>
+	
+    </table>
+	
+<footer>
+	
+	<ul id="footer">
+  		<li><a>@2019 Copyright Live&Play Group</a></li>
+  		<li><a>41st Street, New York, NY 10125</a></li>
+  		
+  		<li><a href="http://maps.google.com/?q=West 41st Street, New York, NY" target="_blank">Map</a></li>
+  		<li style="float:right"><a class="active" href="#">Social</a></li>
+	</ul>
+</footer>
